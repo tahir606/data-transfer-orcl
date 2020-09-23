e65d9ec0ff26370e317d5aa754521d74d36066e1
Tahir <tahir60652@gmail.com>
2020-09-23 18:02:27 +0500

    data-transfer__2020-09-23_(18-02-27)

---
+++ src/main/java/com/bits/datatransfer/authentication/UIAuthentication.java
@@ -0,0 +1,27 @@
+package com.bits.datatransfer.authentication;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/login")
+public class UIAuthentication {
+
+    public UIAuthentication() {
+    }
+
+    @PostMapping
+    public String login(@RequestBody String json) {
+        System.out.println(json);
+        return "{\"token\":\"1234\",\"user\":\"admin\"}";
+    }
+
+    @GetMapping
+    public String verify(@RequestParam String token) {
+        System.out.println(token);
+        if (token.equals("1234"))
+            return "{\"token\":\"1234\",\"user\":\"admin\"}";
+        return "{}";
+    }
+
+}
